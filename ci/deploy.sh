1cb951ab63d86cd9075bc52b4edd6f760d71dbcd
Jaeyong Sung <jaeyong0201@gmail.com>
2020-08-20 18:36:28 +0900

    added ci

---
+++ ci/deploy.sh
@@ -0,0 +1,23 @@
+set -ex
+
+cargo build --release
+cd target/release
+
+case $1 in
+  ubuntu* | macos*)
+    bin="wiki-generator-$1.tar.gz"
+    tar czf ../../"$bin" mdbook
+    ;;
+  windows*)
+    bin="wiki-generator-$1.zip"
+    7z a ../../"$bin" mdbook.exe
+    ;;
+  *)
+    echo "not supported os"
+    ;;
+esac
+
+cd ../..
+
+TAG=${GITHUB_REF#*/tags/}
+hub release edit -m "" --attach "$bin" "$TAG"
