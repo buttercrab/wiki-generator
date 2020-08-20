a0caa3db17a0a3e65dc4c4ce10e424031433a63a
Jaeyong Sung <jaeyong0201@gmail.com>
2020-08-20 21:07:41 +0900

    fixed ci

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,19 +4,8 @@ rustup default nightly
 cargo build --release
 cd target/release
 
-case $1 in
-ubuntu* | macos*)
-  bin="wiki-generator-$1.tar.gz"
-  tar czf ../../"$bin" mdbook
-  ;;
-windows*)
-  bin="wiki-generator-$1.zip"
-  7z a ../../"$bin" mdbook.exe
-  ;;
-*)
-  echo "not supported os"
-  ;;
-esac
+bin="wiki-generator-$1.tar.gz"
+tar czf ../../"$bin" mdbook
 
 cd ../..
 
